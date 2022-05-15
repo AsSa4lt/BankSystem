35ce34b0d7818905a226263e96becade89c77730
Rostyslav <st1tchqwerty@gmail.com>
2022-05-15 11:24:58 +0200

    Initial commit

---
+++ src/com/st1tchqwerty/Main.java
@@ -0,0 +1,10 @@
+package com.st1tchqwerty;
+
+public class Main {
+    public static void main(String[] args) { // TODO Auto-generated method stub
+        Account account = new Account(200, "login", 123);
+        SingleThread stOne = new SingleThread(account); SingleThread stTwo = new SingleThread(account);
+        stOne.getMoneyFromAccount("login", 123, 150);
+        stTwo.getMoneyFromAccount("login", 123, 150);
+    }
+}
